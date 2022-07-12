71f2c61c7562f0bbf35aa06a8713eb288ad0847f
alexlu07 <alexlu.manhasset@gmail.com>
2022-07-12 19:56:35 -0400

    changed camelot

--- training/camelot/camelot.java
+++ training/camelot/camelot.java
@@ -9,7 +9,7 @@ import java.util.*;
 public class camelot {
     public static int r;
     public static int c;
-    public static int[][] directions = new int[8][2];
+    public static int[][] directions = new int[][] { {1, 2}, {2, 1}, {2, -1}, {1, -2}, {-1, -2}, {-2, -1}, {-2, 1}, {-1, 2} };
 
     public static void main(String args[]) throws IOException {
         BufferedReader fin = new BufferedReader(new FileReader("camelot.in"));
@@ -22,9 +22,9 @@ public class camelot {
         king[0] = st.nextToken().charAt(0) - 'A';
         king[1] = Integer.parseInt(st.nextToken()) - 1;
 
+        int[][] totalCost = new int[r][c];
+        int[][] extraCost = new int[r][c];
 
-        int n = 0;
-        ArrayList<int[]> knights = new ArrayList<int[]>();
         String line;
         while ((line = fin.readLine()) != null) {
             st = new StringTokenizer(line);
@@ -32,47 +32,9 @@ public class camelot {
                 int i = st.nextToken().charAt(0) - 'A';
                 int j = Integer.parseInt(st.nextToken())-1;
 
-                knights.add(new int[]{ i, j });
-                n++;
+                search(i, j, totalCost, extraCost);
             }
-
         }
-
-        for (int ind = 0; ind < 8; ind++) {
-            int i = ind / 4;
-            int j = (ind % 4) / 2;
-            int k = ind % 2;
-
-            int a = 1;
-            int b = 2;
-
-            if (i == 1) { a = -1; }
-            if (j == 1) { b = -2; }
-            if (k == 1) {
-                int x = a;
-                a = b;
-                b = x;
-            }
-
-            directions[ind][0] = a;
-            directions[ind][1] = b;
-        }
-
-        HashMap<int[], int[][]> distance = new HashMap<int[], int[][]>();
-        for (int[] knight : knights) {
-            bfs(distance, knight, directions);
-        }
-
-        // for (int[] name: distance.keySet()) {
-        //     String key = Arrays.toString(name);
-        //     System.out.println(key);
-        //     int[][] value = distance.get(name);
-        //     for (int[] row : value) {
-        //         System.out.println(Arrays.toString(row));
-        //     }
-        //     System.out.println("--------------");
-        // }
-
 
 
         PrintWriter fout = new PrintWriter(new BufferedWriter(new FileWriter("camelot.out")));
@@ -80,14 +42,12 @@ public class camelot {
         fout.close();
     }
 
-    public static void bfs(HashMap<int[], int[][]> distance, int[] source, int[][] directions) {
-        int[][] dist = new int[r][c];
+    public static void search(int kI, int kJ, int[][] totalCost, int[][] extraCost) {
 
-        HashSet<Integer> visited = new HashSet<Integer>();
-        visited.add(source[0] * 100 + source[1]);
+        int[][][] visited = new int[r][c][2];
 
-        LinkedList<int[]> queue = new LinkedList<int[]>();
-        queue.add(new int[]{ source[0], source[1], 0 });
+        PriorityQueue<int[]> queue = new PriorityQueue<int[]>  ();
+        queue.add(new int[]{ kI , kJ, 0 });
 
         while (queue.size() > 0) {
             int[] i = queue.poll();
